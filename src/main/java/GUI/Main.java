83ff4e094dd7b7a38473e189d3d3381bd274c569
Adelinn77 <adelinvancea2004@gmail.com>
2025-03-18 20:39:09 +0200

    +pachete

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -1,11 +1,9 @@
 package org.example;
 
+import DataAcces.Model;
+import GUI.View;
+
 import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
